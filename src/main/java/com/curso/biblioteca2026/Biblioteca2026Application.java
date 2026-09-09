5cd33d89ab9f267f309a2e34d374595eca66dda5
Nicolas ELmer Oliveira Souza <nelmer997@gmail.com>
2026-09-09 13:26:02 -0300

    Projeto inicial da biblioteca

---
+++ src/main/java/com/curso/biblioteca2026/Biblioteca2026Application.java
@@ -0,0 +1,13 @@
+package com.curso.biblioteca2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblioteca2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biblioteca2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/biblioteca2026/Biblioteca2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.biblioteca2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biblioteca2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
